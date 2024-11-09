5b4de7af495678091c30f103d61bce6f3734a673
okkun <okuda4645@gmail.com>
2024-11-09 13:29:42 +0900

    fix: 🐛 repository name

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath:
     process.env.NODE_ENV === "production"
-      ? "/your-repo-name" // 本番環境（GitHub Pages）用
+      ? "/tskaigi2025-web-site" // 本番環境（GitHub Pages）用
       : "", // 開発環境用
   images: {
     unoptimized: true,
