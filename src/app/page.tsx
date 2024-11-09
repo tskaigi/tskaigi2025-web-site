a0febecebcfffbb9c3b1305e8ee3ec7fee9645b8
okkun <okuda4645@gmail.com>
2024-11-09 14:22:44 +0900

    style: 💄 add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="grid place-items-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-4xl font-bold">TSKaigi 2025</h1>
+      <Button>準備中</Button>
     </div>
   );
 }
