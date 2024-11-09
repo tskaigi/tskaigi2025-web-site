de55ff1110d2ae6015cd452154d309e45bb7f782
okkun <okuda4645@gmail.com>
2024-11-09 13:06:20 +0900

    fix: 🐛 image bug

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-  basePath: "/tskaigi2025-web-site",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/your-repo-name" // 本番環境（GitHub Pages）用
+      : "", // 開発環境用
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
