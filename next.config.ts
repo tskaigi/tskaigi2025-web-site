47ad387bdb99d5ac004ff81b3a5d5e2939214db5
Yoshiteru Takeshita <takezoux2@gmail.com>
2024-11-22 02:11:07 +0900

    Fix basePath

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production"
-      ? "/tskaigi2025-web-site" // 本番環境（GitHub Pages）用
-      : "", // 開発環境用
+  basePath: "", // 開発環境用
   images: {
     unoptimized: true,
   },
