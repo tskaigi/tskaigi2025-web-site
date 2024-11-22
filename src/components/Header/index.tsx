22b6edd4ccf0bea9e29d5a0009e41bbc9e2fda4a
okkun <okuda4645@gmail.com>
2024-11-23 00:27:44 +0900

    feat: 🎸 add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 w-full p-4 lg:p-8 bg-background">
+      <img src="/logo.svg" className="w-40 lg:w-64" alt="logo" />
+    </header>
+  );
+}
