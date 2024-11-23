f00607d86d4f09562e778304e9066968d769d2a9
okkun <okuda4645@gmail.com>
2024-11-23 18:07:02 +0900

    仮実装

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -4,14 +4,12 @@ export function MainContent() {
   return (
     <div className="relative overflow-hidden w-full">
       <div
-        className="absolute inset-[-50%] w-[200%] h-[200%]"
+        className="absolute inset-[-50%] w-[200%] h-[200%] opacity-15 bg-[length:42px_42px] lg:bg-[length:55px_55px]"
         style={{
           backgroundImage: `
             linear-gradient(hsl(var(--primary)) 2px, transparent 2px),
             linear-gradient(90deg, hsl(var(--primary)) 2px, transparent 2px)
           `,
-          backgroundSize: "55px 55px",
-          opacity: 0.15,
           mask: `radial-gradient(
             ellipse 40% 50% at center,
             black 0%,
