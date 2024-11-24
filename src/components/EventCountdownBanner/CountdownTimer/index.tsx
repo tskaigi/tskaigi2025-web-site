e93b68d34626a2975652ad2c9557c3ef7f92b6cb
okkun <okuda4645@gmail.com>
2024-11-24 16:29:10 +0900

    style: 💄 add bg

--- src/components/EventCountdownBanner/CountdownTimer/index.tsx
+++ src/components/EventCountdownBanner/CountdownTimer/index.tsx
@@ -8,8 +8,25 @@ export function CountdownTimer() {
   const { timeLeft } = useCountdownTimer();
 
   return (
-    <div className="w-[330px] h-[142px] md:w-[600px] md:h-[154px] lg:w-[630px] lg:h-[182px] bg-primary rounded-lg flex items-center justify-center">
-      <div className="flex items-start gap-4 text-white">
+    <div className="relative w-[330px] h-[142px] md:w-[600px] md:h-[154px] lg:w-[630px] lg:h-[182px] bg-primary rounded-lg flex items-center justify-center shadow-[0_4px_6px_rgba(0,0,0,0.09)]">
+      <div
+        className="absolute inset-0 opacity-10 bg-[length:24px_24px] lg:bg-[length:32px_32px]"
+        style={{
+          backgroundImage: `
+            linear-gradient(#fff 1px, transparent 1px),
+            linear-gradient(90deg, #fff 1px, transparent 1px)
+          `,
+          mask: `
+            linear-gradient(135deg, black, transparent 30%),
+            linear-gradient(315deg, black, transparent 30%)
+          `,
+          WebkitMask: `
+            linear-gradient(135deg, black, transparent 30%),
+            linear-gradient(315deg, black, transparent 30%)
+          `,
+        }}
+      />
+      <div className="relative flex items-start gap-4 text-white">
         <TimeLeft
           value={timeLeft.days}
           unit="Days"
