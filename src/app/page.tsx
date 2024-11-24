8319632a4954bd96e7c50df190caca91c992ff41
okkun <okuda4645@gmail.com>
2024-11-24 18:38:40 +0900

    feat: 🎸 デザインレビュー用にSeekingSponsorsSectionも表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { EventCountdownBanner } from "@/components/EventCountdownBanner";
+import { SeekingSponsorsSection } from "@/components/SeekingSponsorsSection";
 
 export default function Home() {
   return (
     <main>
       <EventCountdownBanner />
+      <SeekingSponsorsSection />
     </main>
   );
 }
