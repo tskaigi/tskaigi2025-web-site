bcc6f2f8da138f2af88a7cdadb04acefd8f0eb7d
okkun <okuda4645@gmail.com>
2024-11-24 14:41:06 +0900

    style: 💄 fix GridBackground

--- src/components/EventCountdownBanner/GridBackground/index.tsx
+++ src/components/EventCountdownBanner/GridBackground/index.tsx
@@ -2,20 +2,20 @@ export function GridBackground() {
   return (
     <div
       aria-hidden="true"
-      className="absolute inset-[-50%] w-[200%] h-[200%] opacity-15 bg-[length:42px_42px] lg:bg-[length:55px_55px]"
+      className="absolute inset-0 opacity-15 bg-[length:42px_42px] lg:bg-[length:55px_55px] -z-10"
       style={{
         backgroundImage: `
             linear-gradient(hsl(var(--primary)) 2px, transparent 2px),
             linear-gradient(90deg, hsl(var(--primary)) 2px, transparent 2px)
           `,
         mask: `radial-gradient(
-            ellipse 40% 50% at center,
+            ellipse 80% 80% at center,
             black 0%,
             black 20%,
             transparent 50%
           )`,
         WebkitMask: `radial-gradient(
-            ellipse 40% 50% at center,
+            ellipse 80% 80% at center,
             black 0%,
             black 20%,
             transparent 50%
