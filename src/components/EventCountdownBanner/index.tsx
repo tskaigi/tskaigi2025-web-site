ca1983d7626be2900f810badfa45224b15761746
okkun <okuda4645@gmail.com>
2024-11-26 00:10:42 +0900

    style: 💄 font sizeを修正

--- src/components/EventCountdownBanner/index.tsx
+++ src/components/EventCountdownBanner/index.tsx
@@ -15,11 +15,18 @@ export function EventCountdownBanner() {
       <CountdownTimer />
 
       <div className="text-2xl font-semibold space-y-1">
-        <time dateTime="2025-05-23">5/23, 5/24</time>
-        <p>ベルサール神田</p>
+        <time
+          dateTime="2025-05-23"
+          className="text-[28px] leading-[36px] tracking-[-.75%] semibold"
+        >
+          5/23, 5/24
+        </time>
+        <p className="text-[23px] leading-[36px] tracking-[-.75%] font-bold">
+          ベルサール神田
+        </p>
       </div>
 
-      <h1 className="text-5xl font-bold text-primary w-96 lg:w-full">
+      <h1 className="text-[59px] leading-[48px] tracking-[-1.2%] font-bold text-primary w-96 lg:w-full">
         TSKaigi 2025 Coming Soon
       </h1>
     </div>
