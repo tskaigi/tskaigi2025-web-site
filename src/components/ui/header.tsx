748eef607a0c1b2d9004c378202f5bcb914a25d7
Takayuki Miura <takayuki.miura28@gmail.com>
2025-01-02 21:12:51 +0900

    fix: ナビゲーションを削除

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="fixed top-0 w-full bg-white z-50 flex items-center justify-between p-4 md:px-6">
+    <header className="fixed top-0 w-full bg-white z-50 flex items-center justify-between shadow-sm p-4 md:px-6">
       <Link href="/" className="text-blue-600 font-bold text-xl">
         <Image
           src="/logo.svg"
@@ -13,42 +13,6 @@ export function Header() {
           className="w-[78px] h-[28px] md:w-[98px] md:h-[36px]"
         />
       </Link>
-      <nav className="hidden md:block">
-        <ul className="flex gap-6 text-sm font-bold">
-          <li>
-            <Link
-              href="#mission"
-              className="text-link-light hover:text-link-dark"
-            >
-              ミッション
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#proposal"
-              className="text-link-light hover:text-link-dark"
-            >
-              プロポーザル募集
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#sponsors"
-              className="text-link-light hover:text-link-dark"
-            >
-              スポンサー一覧
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#staff"
-              className="text-link-light hover:text-link-dark"
-            >
-              スタッフ
-            </Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
