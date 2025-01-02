73a715783cc0041aab196bc9db3560fee1f8b0be
tsuyuni <tsuyu.lit@gmail.com>
2025-01-02 20:29:08 +0900

    [fix] logo alt attribute

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -7,12 +7,7 @@ const Footer = () => {
     <footer className="text-white text-sm">
       <div className="px-10 py-16 flex flex-col gap-x-16 gap-y-10 max-w-5xl mx-auto md:flex-row md:p-16 lg:gap-x-24">
         <div className="flex flex-col gap-6 lg:w-72">
-          <Image
-            src="/logo-white.svg"
-            alt="TSKaigiのロゴ"
-            width={126}
-            height={41}
-          />
+          <Image src="/logo-white.svg" alt="TSKaigi" width={126} height={41} />
           <p>一般社団法人 TSKaigi Association</p>
         </div>
 
