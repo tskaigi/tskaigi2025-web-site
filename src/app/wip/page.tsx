126ab8e31e998fcc3c6d844d2ea28c71a1602556
Takayuki Miura <takayuki.miura28@gmail.com>
2025-01-07 22:15:49 +0900

    feat: ヒーローセクションとミッションセクションを追加

--- src/app/wip/page.tsx
+++ src/app/wip/page.tsx
@@ -1,9 +1,15 @@
+import { HeroSection } from "../../components/HeroSection";
+import { MissionSection } from "../../components/MissionSection";
 import { Header } from "../../components/ui/header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="pt-16">
+        <HeroSection />
+        <MissionSection />
+      </main>
     </>
   );
 }
