1d0f6f19eacf9bf2b56830704e676dee2b6f45e7
Ryo Ochiai <ryo.ochiai@kemuridama.jp>
2025-01-09 20:04:26 +0900

    :speech_balloon: Update headline

--- src/components/SeekingSponsorsSection/index.tsx
+++ src/components/SeekingSponsorsSection/index.tsx
@@ -8,7 +8,7 @@ export function SeekingSponsorsSection() {
     <div className="w-full flex flex-col items-center gap-16 px-8 pt-10 pb-16">
       <div className="w-full lg:w-[760px] flex flex-col items-center gap-10">
         <h2 className="text-[32px] lg:text-4xl leading-[48px] tracking-[-1.2%] font-bold text-center">
-          スポンサー募集開始
+          スポンサー募集
         </h2>
         <div className="px-0 md:px-10 font-normal text-m leading-[25px] tracking-[-0.75%]">
           <p>
