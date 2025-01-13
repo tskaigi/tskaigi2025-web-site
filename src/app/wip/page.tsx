02f6b62b2b142db6c323e42f83c8b12480a317c7
Takayuki Miura <takayuki.miura28@gmail.com>
2025-01-13 13:50:33 +0900

    fix: ヘッダーの格納先を変更

--- src/app/wip/page.tsx
+++ src/app/wip/page.tsx
@@ -1,6 +1,6 @@
+import { Header } from "../../components/Header";
 import { HeroSection } from "../../components/HeroSection";
 import { MissionSection } from "../../components/MissionSection";
-import { Header } from "../../components/ui/header";
 
 export default function Home() {
   return (
