4318403c2e19efbc0c6aa2c3beee0c1b709e0bd0
Takayuki Miura <takayuki.miura28@gmail.com>
2025-01-18 20:19:23 +0900

    feat: wipページがクローリングされないように設定

--- src/app/wip/page.tsx
+++ src/app/wip/page.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from "next";
 import { Header } from "../../components/Header";
 import { HeroSection } from "../../components/HeroSection";
 import { MissionSection } from "../../components/MissionSection";
+
+export const metadata: Metadata = {
+  robots: "noindex, nofollow",
+};
 
 export default function Home() {
   return (
