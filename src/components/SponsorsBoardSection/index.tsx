4d13f227a8eb7f2b958a0a21355566dcfa708e85
komura-c <37304826+komura-c@users.noreply.github.com>
2025-01-26 03:06:19 +0900

    feat: スポンサーボードのロゴ部分のコンポーネントを作成

--- src/components/SponsorsBoardSection/index.tsx
+++ src/components/SponsorsBoardSection/index.tsx
@@ -1,3 +1,5 @@
+import { sponsorList } from "../../constants/sponsorList";
+import { SponsorsBoardItem } from "./SponsorsBoardItem";
 import { SponsorsBoardTitle } from "./SponsorsBoardTitle";
 
 export function SponsorsBoardSection() {
@@ -8,80 +10,176 @@ export function SponsorsBoardSection() {
         <span className="inline-block">スポンサー各社</span>
       </h2>
 
-      <div className="pb-10 flex flex-col">
+      <div className="pb-8 flex flex-col">
         <SponsorsBoardTitle titleClassName="before:bg-blue-purple-600 after:bg-blue-purple-600">
-          <h3 className="text-blue-purple-600 text-22 md:text-28 leading-normal md:leading-[42px] text-center font-bold font-noto">
+          <h3 className="text-blue-purple-600 text-22 md:text-28 leading-normal md:leading-[42px] font-bold font-noto">
             Platinum Sponsors
           </h3>
         </SponsorsBoardTitle>
-        <div className="text-center">placeholder</div>
+        <div className="pt-6 px-4 md:px-0 grid gap-4 grid-cols-1 md:grid-cols-3 place-items-center">
+          {sponsorList.platinum.map((sponsor) => (
+            <SponsorsBoardItem
+              key={sponsor.id}
+              className="w-[343px] md:w-[218px] lg:w-[389px] h-[162px] md:h-[192px]"
+              src={sponsor.logoImage}
+              alt={sponsor.name}
+              href={sponsor.logoLink}
+              width={389}
+              height={192}
+            />
+          ))}
+        </div>
       </div>
 
-      <div className="pb-10 flex flex-col">
+      <div className="pb-8 flex flex-col">
         <SponsorsBoardTitle titleClassName="before:bg-yellow-600 after:bg-yellow-600">
-          <h3 className="text-yellow-600 text-22 md:text-28 leading-normal md:leading-[42px] text-center font-bold font-noto">
+          <h3 className="text-yellow-600 text-22 md:text-28 leading-normal md:leading-[42px] font-bold font-noto">
             Gold Sponsors
           </h3>
         </SponsorsBoardTitle>
-        <div className="text-center">placeholder</div>
+        <div className="pt-6 px-4 md:px-0 grid gap-4 grid-cols-2 md:grid-cols-4 place-items-center">
+          {sponsorList.gold.slice(0, 7).map((sponsor) => (
+            <SponsorsBoardItem
+              key={sponsor.id}
+              className="w-[163px] md:w-[160px] lg:w-[288px] h-[144px]"
+              src={sponsor.logoImage}
+              alt={sponsor.name}
+              href={sponsor.logoLink}
+              width={288}
+              height={144}
+            />
+          ))}
+        </div>
       </div>
 
-      <div className="pb-10 flex flex-col">
+      <div className="pb-8 flex flex-col">
         <SponsorsBoardTitle titleClassName="before:bg-blue-light-500 after:bg-blue-light-500">
-          <h3 className="text-blue-light-500 text-22 md:text-28 leading-normal md:leading-[42px] text-center font-bold font-noto">
+          <h3 className="text-blue-light-500 text-22 md:text-28 leading-normal md:leading-[42px] font-bold font-noto">
             Silver Sponsors
           </h3>
         </SponsorsBoardTitle>
-        <div className="text-center">placeholder</div>
+        <div className="pt-6 px-4 md:px-0 grid gap-4 grid-cols-3 md:grid-cols-5 place-items-center">
+          {sponsorList.silver.slice(0, 6).map((sponsor) => (
+            <SponsorsBoardItem
+              key={sponsor.id}
+              className="w-[106px] md:w-[124px] lg:w-[227px] h-[112px]"
+              src={sponsor.logoImage}
+              alt={sponsor.name}
+              href={sponsor.logoLink}
+              width={227}
+              height={112}
+            />
+          ))}
+        </div>
       </div>
 
       <div className="flex gap-3 md:gap-6">
-        <div className="w-1/2 pb-10 flex flex-col">
+        <div className="w-1/2 pb-8 flex flex-col">
           <SponsorsBoardTitle titleClassName="before:bg-black-400 after:bg-black-400">
             <h3 className="text-black-400 text-16 md:text-28 leading-[28.8px] md:leading-[42px] font-bold font-noto">
               Event Platform
             </h3>
           </SponsorsBoardTitle>
-          <div className="text-center">placeholder</div>
+          <div className="pt-6 px-3 md:px-0 grid place-items-center">
+            {sponsorList.eventPlatform.map((sponsor) => (
+              <SponsorsBoardItem
+                key={sponsor.id}
+                className="w-[157px] md:w-[306px] lg:w-[564px] h-[112px]"
+                src={sponsor.logoImage}
+                alt={sponsor.name}
+                href={sponsor.logoLink}
+                width={564}
+                height={112}
+              />
+            ))}
+          </div>
         </div>
 
-        <div className="w-1/2 pb-10 flex flex-col">
+        <div className="w-1/2 pb-8 flex flex-col">
           <SponsorsBoardTitle titleClassName="before:bg-black-400 after:bg-black-400">
             <h3 className="text-black-400 text-16 md:text-28 leading-[28.8px] md:leading-[42px] font-bold font-noto">
               Refreshment
             </h3>
           </SponsorsBoardTitle>
-          <div className="text-center">placeholder</div>
+          <div className="pt-6 px-3 md:px-0 grid place-items-center">
+            {sponsorList.refreshment.map((sponsor) => (
+              <SponsorsBoardItem
+                key={sponsor.id}
+                className="w-[157px] md:w-[306px] lg:w-[564px] h-[112px]"
+                src={sponsor.logoImage}
+                alt={sponsor.name}
+                href={sponsor.logoLink}
+                width={564}
+                height={112}
+              />
+            ))}
+          </div>
         </div>
       </div>
 
       <div className="flex gap-3 md:gap-6">
-        <div className="w-1/2 pb-10 flex flex-col">
+        <div className="w-1/2 pb-8 flex flex-col">
           <SponsorsBoardTitle titleClassName="before:bg-black-400 after:bg-black-400">
             <h3 className="text-black-400 text-16 md:text-28 leading-[28.8px] md:leading-[42px] font-bold font-noto">
               Coffee
             </h3>
           </SponsorsBoardTitle>
-          <div className="text-center">placeholder</div>
+          <div className="pt-6 px-3 md:px-0 grid place-items-center">
+            {sponsorList.coffee.map((sponsor) => (
+              <SponsorsBoardItem
+                key={sponsor.id}
+                className="w-[157px] md:w-[306px] lg:w-[564px] h-[112px]"
+                src={sponsor.logoImage}
+                alt={sponsor.name}
+                href={sponsor.logoLink}
+                width={564}
+                height={112}
+              />
+            ))}
+          </div>
         </div>
 
-        <div className="w-1/2 pb-10 flex flex-col">
+        <div className="w-1/2 pb-8 flex flex-col">
           <SponsorsBoardTitle titleClassName="before:bg-black-400 after:bg-black-400">
             <h3 className="text-black-400 text-16 md:text-28 leading-[28.8px] md:leading-[42px] font-bold font-noto">
               Beer
             </h3>
           </SponsorsBoardTitle>
-          <div className="text-center">placeholder</div>
+          <div className="pt-6 px-3 md:px-0 grid place-items-center">
+            {sponsorList.beer.map((sponsor) => (
+              <SponsorsBoardItem
+                key={sponsor.id}
+                className="w-[157px] md:w-[306px] lg:w-[564px] h-[112px]"
+                src={sponsor.logoImage}
+                alt={sponsor.name}
+                href={sponsor.logoLink}
+                width={564}
+                height={112}
+              />
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className="pb-10 flex flex-col">
+      <div className="pb-8 flex flex-col">
         <SponsorsBoardTitle titleClassName="before:bg-orange-400 after:bg-orange-400">
           <h3 className="text-orange-600 text-16 md:text-28 leading-[28.8px] md:leading-[42px] font-bold font-noto">
             Bronze Sponsors
           </h3>
         </SponsorsBoardTitle>
-        <div className="text-center">placeholder</div>
+        <div className="pt-6 px-4 md:px-0 grid gap-2 md:gap-4 grid-cols-4 md:grid-cols-6 place-items-center">
+          {sponsorList.bronze.map((sponsor) => (
+            <SponsorsBoardItem
+              key={sponsor.id}
+              className="w-[79px] md:w-[101px] lg:w-[186px] h-[96px]"
+              src={sponsor.logoImage}
+              alt={sponsor.name}
+              href={sponsor.logoLink}
+              width={186}
+              height={96}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
