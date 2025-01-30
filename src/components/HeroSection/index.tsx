af3fe722119b630a66e6cc17405ddb685c528dd9
Takayuki Miura <takayuki.miura28@gmail.com>
2025-01-30 23:41:34 +0900

    feat: メインビジュアルの最大幅を設定

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -9,6 +9,13 @@ export function HeroSection() {
         alt="TSKaigi 2025のメインビジュアル。2025年5月23日(金) - 24日(土)に開催。"
         priority
         sizes="100vw"
+        className="xl:max-w-[1280px] xl:mx-auto"
+        style={{
+          WebkitMaskImage:
+            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)",
+          maskImage:
+            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)",
+        }}
       />
     </section>
   );
