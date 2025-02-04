5a0d22296e3999d91fc0cb92d6a679eda769ff11
masayaO <masaya.okuda@acesinc.co.jp>
2025-02-04 16:31:58 +0900

    feat(minor): 🎸 トップページを差し替え

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { EventCountdownBanner } from "@/components/EventCountdownBanner";
-import { SeekingSponsorsSection } from "@/components/SeekingSponsorsSection";
+import { ApplyToProposalSection } from "@/components/ApplyToProposalSection";
+import Footer from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/HeroSection";
+import { MissionSection } from "@/components/MissionSection";
+import { SponsorsBoardSection } from "@/components/SponsorsBoardSection";
 
 export default function Home() {
   return (
     <>
-      <main className="w-full flex flex-col items-center font-outfit">
-        <EventCountdownBanner />
-        <SeekingSponsorsSection />
+      <Header />
+      <main className="pt-16">
+        <HeroSection />
+        <MissionSection />
+        <ApplyToProposalSection />
+        <SponsorsBoardSection isTopPage={true} />
       </main>
+      <Footer />
     </>
   );
 }
