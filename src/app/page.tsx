e89100a69d5355427833d898e0ab9619edb10a9d
okkun <okuda4645@gmail.com>
2025-02-17 09:13:08 +0900

    feat: 🎸 すべてのロゴを公開しフィルターを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <HeroSection />
         <MissionSection />
         <ApplyToProposalSection />
-        <SponsorsBoardSection isTopPage={true} />
+        <SponsorsBoardSection />
       </main>
       <Footer />
     </>
