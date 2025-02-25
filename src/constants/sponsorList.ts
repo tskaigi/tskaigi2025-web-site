9fd8b8a6d3fa0f9dd16e9a6ab7ba48073ba0732b
okkun <okuda4645@gmail.com>
2025-02-25 19:45:35 +0900

    feat: 🎸 add ascend

--- src/constants/sponsorList.ts
+++ src/constants/sponsorList.ts
@@ -28,6 +28,24 @@ export const sponsorList: SponsorList = {
       logoLink: "https://www.ascendlogi.co.jp/",
       addPadding: false,
       isTenantChecked: true,
+      overview: [
+        '■「日本で最もデジタル化の遅れた物流産業で、最高の業務体験を創る」\nアセンド株式会社は日本の物流DXを目指すスタートアップです。\n"物流業界は20兆円の巨大市場でありながら、クラウド利用率は全産業で最低でありデジタル化の遅れ故に2024年問題をはじめとして社会課題を抱える業界です。アセンドはVertical SaaS「ロジックス」を全国の運送会社へ開発提供し、現場業務のデジタル化と経営の高度化を同時に図り、物流業界の変革を推進しています。',
+        "■ アセンドとTypeScript\nアセンドは社会課題を解決するプロダクトエンジニア組織を目指して、Frontend/Backend/IaC/DataPipelineの全てをTypeScriptで統一しました。TSでの言語統一により、全エンジニアがフルスタックに開発し、顧客業務やドメインに深く寄り添いプロダクト価値に拘った開発を進めています。開発生産性は6deploys/dayと高い環境であり、少数チームでありながらサービスは案件管理や労務・車両管理といった多岐にわたる複数プロダクトを展開し、運送会社の全ての業務のデジタル化を推進しています。2023年12月にはシリーズAの資金調達を実施し、より多くの運送会社に更なる機能・価値を届けるため、仲間を募集しています。\nTypeScriptを駆使して社会課題解決に挑むプロダクトエンジニアを積極募集中です。お気軽にご連絡ください！",
+      ],
+      links: [
+        {
+          title: "アセンド株式会社",
+          href: "https://www.ascendlogi.co.jp/",
+        },
+        {
+          title: "エンジニア採用サイト",
+          href: "https://product.career.ascendlogi.co.jp/",
+        },
+        {
+          title: "プロダクトエンジニアに関して",
+          href: "https://note.com/niwa_takeru/n/n0ae4acf2964d",
+        },
+      ],
       roles: ["platinum"],
     },
     {
