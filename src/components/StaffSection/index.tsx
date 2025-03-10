dbaf13798401a68a5c878d69e1c5be4536850390
shoco55 <51412333+shoco55@users.noreply.github.com>
2025-03-10 22:22:39 +0900

    feat: タイトルサイズや余白調整

--- src/components/StaffSection/index.tsx
+++ src/components/StaffSection/index.tsx
@@ -3,10 +3,10 @@ import { staffList } from "@/constants/staffList";
 export function StaffSection() {
   return (
     <section className="pb-10 md:pb-20 bg-blue-light-100">
-      <h2 className="text-24 md:text-28 leading-normal md:leading-[42px] text-center font-bold pb-8 md:pb-10">
+      <h2 className="text-24 lg:text-28 leading-normal text-center font-bold pb-8 md:pb-10">
         スタッフ一覧
       </h2>
-      <ul className="grid grid-cols-[repeat(auto-fit,120px)] md:grid-cols-[repeat(4,120px)] lg:grid-cols-[repeat(5,120px)] justify-center gap-x-14 gap-y-8 px-10">
+      <ul className="grid grid-cols-[repeat(auto-fit,120px)] md:grid-cols-[repeat(4,120px)] lg:grid-cols-[repeat(5,120px)] justify-center gap-x-14 gap-y-14 md:gap-y-8 px-10">
         {staffList.map(({ name, image, href }) => (
           <li key={name}>
             <a
