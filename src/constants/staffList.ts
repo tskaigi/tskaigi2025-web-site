ad7163c93d097a6b564a72a44090983cc2919017
shoco55 <51412333+shoco55@users.noreply.github.com>
2025-03-15 19:32:22 +0900

    fix: スタッフの重複を排除 & スタッフ一覧セクションをコメントアウトで非表示にする

--- src/constants/staffList.ts
+++ src/constants/staffList.ts
@@ -20,34 +20,4 @@ export const staffList: Staff[] = [
     image: "/staff/shoko.jpg",
     href: "https://x.com/shoco_shoco55",
   },
-  {
-    name: "okkun",
-    image: "/staff/okkun.jpg",
-    href: "https://x.com/masaya_okuda",
-  },
-  {
-    name: "tocomi",
-    image: "/staff/tocomi.png",
-    href: "https://x.com/tocomi0112",
-  },
-  {
-    name: "shoko",
-    image: "/staff/shoko.jpg",
-    href: "https://x.com/shoco_shoco55",
-  },
-  {
-    name: "okkun",
-    image: "/staff/okkun.jpg",
-    href: "https://x.com/masaya_okuda",
-  },
-  {
-    name: "tocomi",
-    image: "/staff/tocomi.png",
-    href: "https://x.com/tocomi0112",
-  },
-  {
-    name: "shoko",
-    image: "/staff/shoko.jpg",
-    href: "https://x.com/shoco_shoco55",
-  },
 ];
