9dfb14c38d463c1da68f478c502ae3a74e0e8f4a
ningen <ningen361@gmail.com>
2025-03-23 01:48:11 +0900

    feat: 更新済のデザインに修正した

---
+++ src/components/ui/sectionGradation.tsx
@@ -0,0 +1,17 @@
+type SectionGradationProps = {
+  children: React.ReactNode;
+};
+
+export const SectionGradation = ({ children }: SectionGradationProps) => {
+  return (
+    <section
+      style={{
+        background:
+          "linear-gradient(180deg, rgba(133, 196, 255, 0.6) 0%, rgba(202, 230, 255, 0.6) 5.28%, rgba(233, 245, 255, 0.6) 8.92%)",
+      }}
+      className="relative z-10"
+    >
+      {children}
+    </section>
+  );
+};
