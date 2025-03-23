fab1f423cb0328fa735d9042a719645ff041b6e0
okkun <okuda4645@gmail.com>
2025-03-24 08:47:58 +0900

    feat: 🎸 add EventDateTab

--- src/app/wip/talks/page.tsx
+++ src/app/wip/talks/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { EventDateTab } from "@/components/talks/EventDateTab";
+import { useState } from "react";
+import type { EventDate } from "@/constants/talkList";
+
 const TalksPage = () => {
+  const [currentDate, setCurrentDate] = useState<EventDate>("DAY1");
+
+  const handleTabChange = (date: EventDate) => {
+    setCurrentDate(date);
+    // 必要に応じて他のロジックを追加
+  };
   return (
-    <main className="bg-blue-light-100 py-16 px-10">
-      <h1 className="text-2xl font-bold text-blue-light-500 text-center py-10 md:py-16 md:text-3xl lg:text-4xl">
+    <main className="bg-blue-light-100 mt-16 py-16 px-10">
+      <h1 className="text-2xl font-bold text-blue-light-500 text-center md:text-3xl lg:text-4xl">
         タイムテーブル
       </h1>
+      <div className="text-center mt-8">
+        <EventDateTab currentDate={currentDate} onTabChange={handleTabChange} />
+      </div>
     </main>
   );
 };
--- src/components/talks/EventDateTab/index.tsx
+++ src/components/talks/EventDateTab/index.tsx
@@ -1,9 +1,37 @@
 import type { EventDate } from "@/constants/talkList";
+import { EVENT_DATE } from "@/constants/talkList";
+import { cn } from "@/lib/utils";
 
 type Props = {
   currentDate: EventDate;
+  onTabChange: (date: EventDate) => void;
 };
 
-export function EventDateTab({ currentDate }: Props) {
-  return <></>;
+export function EventDateTab({ currentDate, onTabChange }: Props) {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="inline-flex rounded-md overflow-hidden">
+        {(Object.keys(EVENT_DATE) as EventDate[]).map((date) => {
+          const isActive = date === currentDate;
+          const dateText = date === "DAY1" ? "Day 1  5/23" : "Day 2  5/24";
+
+          return (
+            <button
+              type="button"
+              key={date}
+              onClick={() => onTabChange(date)}
+              className={cn(
+                "px-6 py-2 text-sm font-medium transition-colors",
+                isActive
+                  ? "bg-blue-light-500 text-white"
+                  : "bg-black-300 text-white hover:bg-blue-light-400 ",
+              )}
+            >
+              {dateText}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
