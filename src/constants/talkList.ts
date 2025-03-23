3f11722cc405e09aa31ec2a6f5829c0552213cf6
okkun <okuda4645@gmail.com>
2025-03-23 18:30:03 +0900

    feat: 🎸 定数を定義

---
+++ src/constants/talkList.ts
@@ -0,0 +1,43 @@
+const EVENT_DATE = {
+  DAY1: "2025-03-23",
+  DAY2: "2025-03-24",
+} as const;
+type EventDate = keyof typeof EVENT_DATE;
+
+const TRACK = {
+  TRACK1: {
+    name: "トグルホールディングストラック",
+  },
+  TRACK2: {
+    name: "アセンドトラック",
+  },
+  TRACK3: {
+    name: "レジレジーズトラック",
+  },
+};
+type Track = keyof typeof TRACK;
+
+const TalkType = {
+  SESSION: {
+    name: "セッション",
+  },
+  KEYNOTE: {
+    name: "基調講演",
+  },
+  LT: {
+    name: "LT",
+  },
+  SPONSOR_LT: {
+    name: "スポンサーLT",
+  },
+};
+type TalkType = keyof typeof TalkType;
+
+type Talk = {
+  id: string;
+  eventDate: EventDate;
+  track: Track;
+  talkType: TalkType;
+  title: string;
+  speaker: string;
+};
