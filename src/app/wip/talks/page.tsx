2e8a0c4c31c0c42d55eacfae7d3a1c08cca51e18
okkun <okuda4645@gmail.com>
2025-03-24 08:17:26 +0900

    feat: 🎸 add wip page

---
+++ src/app/wip/talks/page.tsx
@@ -0,0 +1,11 @@
+const TalksPage = () => {
+  return (
+    <main className="bg-blue-light-100 py-16 px-10">
+      <h1 className="text-2xl font-bold text-blue-light-500 text-center py-10 md:py-16 md:text-3xl lg:text-4xl">
+        タイムテーブル
+      </h1>
+    </main>
+  );
+};
+
+export default TalksPage;
