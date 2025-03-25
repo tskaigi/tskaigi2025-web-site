597be2e48f7f9b645ad8df569403fafee9ff364f
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-03-25 21:27:39 +0900

    fix: typo sponser -> sponsor

--- src/app/wip/sponsors/[id]/page.tsx
+++ src/app/wip/sponsors/[id]/page.tsx
@@ -36,7 +36,7 @@ export async function generateMetadata({
   };
 }
 
-export default async function SponserDetailPage({
+export default async function SponsorDetailPage({
   params,
 }: {
   params: Promise<{ id: string }>;
