f8b5cca0ad7fcb301a9bb02712b9ec26e771ee29
tsuyuni <yuri.tsuchikawa@gmail.com>
2025-03-27 18:45:35 +0900

    Revert "[update] page min hight"
    This reverts commit 4db6b420e8d8a9ac8a6d444f15b26981565a2c70.

--- src/app/wip/sponsors/layout.tsx
+++ src/app/wip/sponsors/layout.tsx
@@ -7,12 +7,10 @@ type Props = {
 
 export default function SponsorLayout({ children }: Props) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
-      <main className="bg-blue-light-100 pt-16 py-10 md:px-8 flex-1">
-        {children}
-      </main>
+      <main className="bg-blue-light-100 pt-16 py-10 md:px-8">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 }
