2ef3f2ace6123a0c5eefc09fd4faf65676fe6ef6
masayaO <masaya.okuda@acesinc.co.jp>
2025-04-01 14:54:58 +0900

    feat(minor): 🎸 EventWrapperを修正

--- src/components/talks/EventWrapper/index.tsx
+++ src/components/talks/EventWrapper/index.tsx
@@ -1,5 +1,6 @@
-import type { TalkType } from "@/constants/talkList";
+import { TRACK, type TalkType } from "@/constants/talkList";
 import type { Track } from "@/constants/talkList";
+import { cn } from "@/lib/utils";
 import type React from "react";
 import type { CSSProperties } from "react";
 import { TalkTypeLabel } from "./TalkTypeLabel";
@@ -20,8 +21,6 @@ export function EventWrapper({
   track,
 }: Props) {
   const getTriangleStyle = (): CSSProperties => {
-    if (!track) return {};
-
     const color =
       track === "TRACK1"
         ? "var(--track-toggle)"
@@ -41,11 +40,45 @@ export function EventWrapper({
     } as CSSProperties;
   };
 
+  const getBgColorClass = (() => {
+    switch (track) {
+      case "TRACK1":
+        return "bg-track-toggle";
+      case "TRACK2":
+        return "bg-track-ascend";
+      case "TRACK3":
+        return "bg-track-leverages";
+      default:
+        return track satisfies never;
+    }
+  })();
+  const textColorClass = () => {
+    switch (track) {
+      case "TRACK1":
+        return "text-black";
+      case "TRACK2":
+        return "text-white";
+      case "TRACK3":
+        return "text-white";
+      default:
+        return track satisfies never;
+    }
+  };
+
   return (
     <div
-      className={`${color === "gray" ? "bg-gray-200" : "bg-white"} p-5 min-h-32 flex flex-col gap-2 items-${textAlign === "center" ? "center" : "start"} justify-center ${talkType ? "lg:justify-start" : "lg:justify-center"} text-black-700 relative`}
+      className={`${color === "gray" ? "bg-gray-200" : "bg-white"} px-5 pt-10 pb-4 md:py-5 min-h-32 flex flex-col gap-2 items-${textAlign === "center" ? "center" : "start"} justify-center ${talkType ? "lg:justify-start" : "lg:justify-center"} text-black-700 relative`}
     >
-      {track && <div style={getTriangleStyle()} />}
+      <div
+        className={cn(
+          getBgColorClass,
+          textColorClass(),
+          "block md:hidden py-1 px-2 absolute top-0 left-0 text-xs font-bold",
+        )}
+      >
+        {TRACK[track].name}
+      </div>
+      <div className="hidden md:block" style={getTriangleStyle()} />
       {talkType && <TalkTypeLabel talkType={talkType} />}
       <div
         className={`${textAlign === "center" ? "text-center" : "text-left"}`}
