8f53e6f3758deda492bcf7b569e73f291080bb5e
ningen <ningen361@gmail.com>
2025-05-01 12:12:48 +0900

    refactor: id 列を追加

--- src/constants/personalSponsorList.ts
+++ src/constants/personalSponsorList.ts
@@ -1,49 +1,59 @@
 export const personalSponsorList: {
+  id: number;
   name: string;
   link: string;
   imgSrc: string;
 }[] = [
   {
+    id: 1,
     name: "おおいし (bicstone)",
     link: "https://x.com/bicstone_me",
     imgSrc: "/personalSponsors/bicstone_me.webp",
   },
   {
+    id: 2,
     name: "パンダム/rymiyamoto",
     link: "https://x.com/rymiyamoto129",
     imgSrc: "/personalSponsors/rymiyamoto129.webp",
   },
   {
+    id: 3,
     name: "みっちー",
     link: "https://x.com/michiya_nishida",
     imgSrc: "/personalSponsors/michiya_nishida.webp",
   },
   {
+    id: 8,
     name: "fukumura",
     link: "https://x.com/fukumura",
     imgSrc: "/personalSponsors/fukumura.webp",
   },
   {
+    id: 9,
     name: "kzhrk",
     link: "https://x.com/kzhrk0430",
     imgSrc: "/personalSponsors/kzhrk0430.webp",
   },
   {
+    id: 7,
     name: "Natsuki",
     link: "https://x.com/natch_engr",
     imgSrc: "/personalSponsors/natch_engr.webp",
   },
   {
+    id: 6,
     name: "onk",
     link: "https://x.com/onk",
     imgSrc: "/personalSponsors/onk.webp",
   },
   {
+    id: 4,
     name: "shige",
     link: "https://codenote.net/about/",
     imgSrc: "/personalSponsors/codenote_net.webp",
   },
   {
+    id: 5,
     name: "Yuji Yamaguchi",
     link: "https://github.com/yug1224",
     imgSrc: "/personalSponsors/yug1224.webp",
