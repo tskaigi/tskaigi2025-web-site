1c4ecdf2d68bfb8d040df606f00fae444c3838a0
tsuyuni <yuri.tsuchikawa@gmail.com>
2025-05-13 01:56:31 +0900

    [feat] for sp style

--- src/components/talks/TimeSlot/index.tsx
+++ src/components/talks/TimeSlot/index.tsx
@@ -4,8 +4,10 @@ type Props = {
 
 export function TimeSlot({ timeText }: Props) {
   return (
-    <div className="bg-yellow-200 py-2 px-1 flex items-center justify-center text-center w-full md:w-[99px] lg:w-[125px] text-sm font-medium text-yellow-700">
-      {timeText}
+    <div className="sticky top-14 z-30 shadow shadow-blue-light-100 pt-2 bg-blue-light-100">
+      <div className="bg-yellow-200 py-2 px-1 flex items-center justify-center text-center w-full md:w-[99px] lg:w-[125px] text-sm font-medium text-yellow-700">
+        {timeText}
+      </div>
     </div>
   );
 }
