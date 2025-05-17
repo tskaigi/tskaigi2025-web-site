bc461cb12bee8cfb88514288c75c24b6847614b5
shoco55 <51412333+shoco55@users.noreply.github.com>
2025-05-17 16:04:43 +0900

    feat: 現在時刻のタイムスロットだけUIを変える（ひとまずday1だけ実装）

--- src/components/talks/TimeSlot/index.tsx
+++ src/components/talks/TimeSlot/index.tsx
@@ -1,11 +1,25 @@
 type Props = {
+  isOngoing?: boolean;
   timeText: string;
 };
 
-export function TimeSlot({ timeText }: Props) {
+export function TimeSlot({ isOngoing = false, timeText }: Props) {
   return (
-    <div className="bg-yellow-200 py-2 px-1 flex items-center justify-center text-center w-full md:w-[99px] lg:w-[125px] text-sm font-medium text-yellow-700">
-      {timeText}
+    <div
+      className={`relative flex flex-col gap-2 p-2 items-center justify-center text-center w-full md:w-[99px] lg:w-[125px] ${
+        isOngoing
+          ? "bg-orange-200 after:content-[''] after:absolute after:top-0 after:right-0 after:w-[5px] after:h-full after:bg-orange-500 after:opacity-50"
+          : "bg-yellow-200"
+      }`}
+    >
+      <p
+        className={`text-sm lg:text-base font-bold ${
+          isOngoing ? "text-orange-500" : "text-yellow-700"
+        }`}
+      >
+        {timeText}
+      </p>
+      {isOngoing && <p className="text-xs text-orange-500 font-normal">now</p>}
     </div>
   );
 }
