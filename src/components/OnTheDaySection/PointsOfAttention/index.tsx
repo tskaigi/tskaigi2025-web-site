d8d025a335b4fd1dabc95af744c64fb4f2942b3e
kryota-dev <50436249+kryota-dev@users.noreply.github.com>
2025-05-21 22:18:31 +0900

    feat: フォントサイズ調整

--- src/components/OnTheDaySection/PointsOfAttention/index.tsx
+++ src/components/OnTheDaySection/PointsOfAttention/index.tsx
@@ -5,7 +5,7 @@ import { Caution } from "../Notes/Caution";
  */
 export function PointsOfAttention() {
   return (
-    <div className="flex flex-col p-6 border border-blue-500 rounded-xl gap-3">
+    <div className="flex flex-col p-6 border border-blue-500 rounded-xl gap-3 text-lg max-md:text-base">
       <Caution>
         会場にはwifi(id,passはお手持ちのリーフレット参照)がありますのでご自由にお使いください。ただし、遅延を避けるためdocker
         pull等、負荷が懸念される操作はご遠慮ください。
