e480a2c12047ede0f67eb9c6c4a3950f813b317d
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-05-21 19:27:50 +0900

    fix: :recycle: roleを設定する箇所の変更とaria-labeledbyの追加

--- src/components/OnTheDaySection/UrgentAnnouncement/index.tsx
+++ src/components/OnTheDaySection/UrgentAnnouncement/index.tsx
@@ -7,7 +7,14 @@ import { LinkText } from "@/components/OnTheDaySection/Notes/LinkText";
  */
 export function UrgentAnnouncement() {
   return (
-    <div className="bg-white w-full flex flex-col gap-3 justify-center rounded-lg md:rounded-2xl px-6 py-5 md:px-8 md:py-6">
+    <section
+      className="bg-white w-full flex flex-col gap-3 justify-center rounded-lg md:rounded-2xl px-6 py-5 md:px-8 md:py-6"
+      role="alert"
+      aria-labelledby="alert_description"
+    >
+      <span id="alert_description" className="sr-only">
+        緊急のお知らせ
+      </span>
       <Caution>
         ダミーの緊急のお知らせです。詳細は
         <LinkText href="https://example.com" target="_blank">
@@ -21,6 +28,6 @@ export function UrgentAnnouncement() {
           ダミーの緊急のお知らせです。
         </LinkText>
       </Caution>
-    </div>
+    </section>
   );
 }
