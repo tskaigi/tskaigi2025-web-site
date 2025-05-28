57d97c4705d40518edce7968b346ab4771d5a3d0
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-05-28 23:58:51 +0900

    chore: cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,14 @@ import { HeroSectionWithMotion } from "@/components/HeroSectionWithMotion";
 import { JobBoardSection } from "@/components/JobBoardSection";
 import { JudgesSection } from "@/components/JudgesSection";
 import { MissionSection } from "@/components/MissionSection";
-import { OnTheDaySection } from "@/components/OnTheDaySection";
 import PersonalSponsorsSection from "@/components/PersonalSponsorsSection";
 import { SponsorsBoardSection } from "@/components/SponsorsBoardSection";
 import { StaffSection } from "@/components/StaffSection";
-import { VenueGuideMapSection } from "@/components/VenueGuideMap";
 
 export default function Home() {
   return (
     <main className="pt-8 overflow-x-hidden">
       <HeroSectionWithMotion />
-      <OnTheDaySection />
-      <VenueGuideMapSection />
       <MissionSection />
       <JobBoardSection />
       <SponsorsBoardSection />
